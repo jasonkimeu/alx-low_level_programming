f3606ea09b2ea3df69314020797812335c026d6a
Jason <jasonkimeu@gmail.com>
2022-09-22 11:43:40 -0700

    999

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
- * @n: The integer to be printed.
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
  */
+
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int tens, dig, pos = n;
+	double temp_tens = 1;
 
-	if (n < 0)
+	if (n == 0)
+		_putchar('0');
+	else
 	{
-		_putchar('-');
-		num = -num;
+		if (n < 0)
+		{
+			pos = n * -1;
+			_putchar('-');
+		}
+
+		while (temp_tens <= pos)
+			temp_tens *= 10;
+		tens = temp_tens / 10;
+
+		while (tens >= 1)
+		{
+			dig = pos / tens;
+			_putchar(dig + '0');
+			pos = (pos - (tens * dig));
+			tens /= 10;
+		}
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
 }
