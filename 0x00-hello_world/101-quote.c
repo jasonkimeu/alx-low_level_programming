1acfc7eecec508d26f1f4e324ff6e7dc63cd9669
Jason <jasonkimeu@gmail.com>
2022-09-28 00:58:48 -0700

    Writes a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char var[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, var, sizeof(var) - 1);
+	return (1);
+}
